cc751df377ef6ba538d06118ec483a801c459a80
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2025-12-17 02:04:22 -0600

    Update search build.gradle

--- search/src/main/java/com/github/scholarfind/Main.java
+++ search/src/main/java/com/github/scholarfind/Main.java
@@ -1,8 +1,11 @@
 package com.github.scholarfind;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public final class Main {
-  
 
   public static final void main(final String... arguments) {
 
