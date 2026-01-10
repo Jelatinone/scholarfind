6d4701d0d23f505c8133c2b10ef22c2059713da4
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-01-10 01:50:46 -0600

    Remove Useless NonNull Check

--- search/src/main/java/com/github/scholarfind/task/signal/SignalExtractionResult.java
+++ search/src/main/java/com/github/scholarfind/task/signal/SignalExtractionResult.java
@@ -2,14 +2,12 @@ package com.github.scholarfind.task.signal;
 
 import java.util.Optional;
 
-import lombok.NonNull;
-
 public sealed interface SignalExtractionResult {
 
-	public static record Both(SignalCost cost, @NonNull Optional<SignalValue> value) implements SignalExtractionResult {
+	public static record Both(SignalCost cost, Optional<SignalValue> value) implements SignalExtractionResult {
 	}
 
-	public static record Value(@NonNull Optional<SignalValue> value) implements SignalExtractionResult {
+	public static record Value(Optional<SignalValue> value) implements SignalExtractionResult {
 	}
 
 	public static record Cost(SignalCost cost) implements SignalExtractionResult {
