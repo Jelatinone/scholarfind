0c1eb3cb05cdc4060db50bcf32e43a816ff5d391
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-05-02 11:59:08 -0500

    Remove Unnecessary Input Queue

--- common/src/main/java/com/github/jelatinone/meta/construct/Infrastructure.java
+++ common/src/main/java/com/github/jelatinone/meta/construct/Infrastructure.java
@@ -1,15 +1,11 @@
 package com.github.jelatinone.meta.construct;
 
-import com.github.jelatinone.api.queue.RetryableQueue;
 import com.github.jelatinone.api.store.Store;
 import com.github.jelatinone.model.audit.AttemptEvent;
 import com.github.jelatinone.model.audit.ExecutionEvent;
 import com.github.jelatinone.model.struct.Request;
-import com.github.jelatinone.model.transit.Letter;
 
 public interface Infrastructure<In extends Request, Out extends Request> extends AutoCloseable {
-
-	RetryableQueue<Letter<In>> input();
 
 	Router router();
 
